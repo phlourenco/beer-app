d39d3b649bd3ff4b48e472cdca411d67e29cf0b6
Paulo Lourenço <paulohll14@gmail.com>
2019-04-11 19:38:17 -0300

    First commit

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'uol-beer-app' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for uol-beer-app
+  pod 'SDWebImage'
+
+  target 'uol-beer-appTests' do
+    inherit! :search_paths
+    # Pods for testing
+    pod 'Cuckoo'
+  end
+
+end
---
+++ uol-beer-appTests/uol_beer_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  uol_beer_appTests.swift
+//  uol-beer-appTests
+//
+//  Created by Paulo Lourenço on 11/04/19.
+//  Copyright © 2019 Paulo Lourenço. All rights reserved.
+//
+
+import XCTest
+@testable import uol_beer_app
+
+class uol_beer_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
